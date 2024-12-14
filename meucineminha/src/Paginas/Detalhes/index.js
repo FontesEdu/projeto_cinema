22748fd73dcb4dba25134cc5bdd2d7ed186204e3
Eduardo Fontes <134387039+FontesEdu@users.noreply.github.com>
2024-12-14 02:16:04 +0000

    Atualização do Estilo

--- meucineminha/src/Paginas/Detalhes/index.js
+++ meucineminha/src/Paginas/Detalhes/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../Serviços/api";
+import "../Estilo/estilo.css";
 
 export default function Detalhes(){
 
@@ -24,18 +25,13 @@ export default function Detalhes(){
 
     return (
         <div>
-            
-                <div key={filmes.id}>
-                    
-                    <h1>{filmes.title}</h1>
-                    <img src= {`https://image.tmdb.org/t/p/w600_and_h900_bestv2${filmes.backdrop_path}`}></img>
-                    <h1>{filmes.overview}</h1>
-                    <h1>{filmes.vote_average}</h1>
-                    <h1>{filmes.release_date}</h1>
-                    
-                </div> 
-            
-            
+            <div key={filmes.id}>
+                <h1>{filmes.title}</h1>
+                <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${filmes.backdrop_path}`} />
+                <h1>{filmes.overview}</h1>
+                <h1>Nota: {filmes.vote_average}</h1>
+                <h1>Lançamento: {filmes.release_date}</h1>
+            </div> 
         </div>
     );
 }
--- meucineminha/src/Paginas/Home/index.js
+++ meucineminha/src/Paginas/Home/index.js
@@ -1,20 +1,19 @@
 import api from "../Serviços/api";
 import { useState, useEffect } from "react";
+import "../Estilo/estilo.css";
 
-export default function Home(){
-    
+export default function Home() {
     const [filmes, setFilmes] = useState([]);
     const [pesquisa, setPesquisa] = useState();
 
-    async function loadFilmes(){
+    async function carregarFilmes() {
         const response = await api.get("movie/now_playing", {
-            params : {
+            params: {
                 api_key: "a61dd6dc589247452f401167670b17a2",
                 language: "pt-BR",
-                page : 1
-            }
-        })
-        console.log(response.data.results);
+                page: 1,
+            },
+        });
         setFilmes(response.data.results);
     }
 
@@ -23,59 +22,52 @@ export default function Home(){
         setPesquisa(e.target[0].value);
     }
 
-    async function Pesquisar(e) {
+    async function pesquisar(e) {
         const response = await api.get(`search/movie?query=${pesquisa}`, {
             params: {
                 api_key: "a61dd6dc589247452f401167670b17a2",
                 language: "pt-BR",
-                page: 1
-            }
-        })
-        console.log(response.data.results);
+                page: 1,
+            },
+        });
         setFilmes(response.data.results);
     }
 
     useEffect(() => {
         if (pesquisa) {
-            Pesquisar();
+            pesquisar();
         } else {
-            loadFilmes();
+            carregarFilmes();
         }
-    
-    }, [pesquisa])
-    
+    }, [pesquisa]);
+
     return (
-        <div>
-
-        <div>
-            <h2>Pesquisar:</h2>
-
-            <form onSubmit={(e) => handleInputChange(e)} className="search">
-                <input type="text" placeholder= "Pesquisar" id="search" />
-                <input type="submit" />
-            </form>
-
-            <br></br>
-            
-        </div> 
-        
-        <div>
-
+        <div className="container">
+            <div className="pesquisa">
+                <h2>Pesquisar:</h2>
+                <form onSubmit={(e) => handleInputChange(e)} className="form-pesquisa">
+                    <input type="text" placeholder="Pesquisar" className="input-pesquisa" />
+                    <input type="submit" className="btn-pesquisa" />
+                </form>
             </div>
-            {filmes.map((filme) => (
-                <div key={filme.id}>
-                    <a href={`/detalhes/${filme.id}`}>
-                    <h1>{filme.title}</h1>
-                    </a>
-                    
-                    <img src= {`https://image.tmdb.org/t/p/w600_and_h900_bestv2${filme.backdrop_path}`}></img>
-                    <h1>{filme.overview}</h1>
-                    <h1>{filme.vote_average}</h1>
-                    <h1>{filme.release_date}</h1>
-                    
-                </div> 
-            ))}
-            <h1>Estou na Home</h1>
+
+            <div className="catalogo">
+                {filmes.map((filme) => (
+                    <div key={filme.id} className="card">
+                        <a href={`/detalhes/${filme.id}`} className="link-titulo">
+                            <h1 className="titulo-filme">{filme.title}</h1>
+                        </a>
+                        <img
+                            src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${filme.backdrop_path}`}
+                            alt={filme.title}
+                            className="imagem-filme"
+                        />
+                        <h1 className="descricao-filme">{filme.overview}</h1>
+                        <h1 className="nota-filme">{filme.vote_average}</h1>
+                        <h1 className="data-lancamento">{filme.release_date}</h1>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
